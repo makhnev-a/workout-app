123b56812fdb015a570315b8d157126a8f6992af
Andrey Makhnev <ray.makhnev@gmail.com>
2022-07-09 15:36:14 +0300

    connect to db

--- back/server.js
+++ back/server.js
@@ -1,17 +1,21 @@
-import express from "express"
-import morgan from "morgan"
-import dotenv from "dotenv"
-import colors from "colors"
+import express from 'express'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import colors from 'colors'
+
+/** Config */
+import { connectDB } from './config/db.js'
 
 /** Routes */
-import userRoutes from "./routes/userRoutes.js";
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
+connectDB()
 
 const app = express()
 
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+	app.use(morgan('dev'))
 }
 
 app.use(express.json())
@@ -19,4 +23,9 @@ app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.PORT} mode on port ${PORT}`.yellow.bold))
+app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.PORT} mode on port ${PORT}`.yellow.bold
+	)
+)
